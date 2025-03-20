c3859471752a2dfcb1cc09c8b533a4bd804c38c4
Hajar Chakloul <158058552+hajarchakloul@users.noreply.github.com>
2025-03-20 16:04:33 +0000

    commit 2

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -17,7 +17,6 @@ class _HomescreenState extends State<Homescreen> {
           leading: Padding(
             padding: const EdgeInsets.only(left: 10),
             child: SizedBox(
-
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(60),
                 child: Image.network(
@@ -93,40 +92,358 @@ class _HomescreenState extends State<Homescreen> {
             Row(
               children: [
                 Expanded(
-                    child: Container(
-                  width: 40,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          blurRadius: 8,
+                          spreadRadius: 2,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment
+                          .start, // جعل المحتوى يبدأ من اليسار
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.blue.shade100,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: const Icon(Icons.inventory_2_outlined,
+                              color: Colors.blue, size: 20),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          "60",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        const Text(
+                          "Out of stock",
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                )),
+                ),
                 const Padding(
                   padding: EdgeInsets.all(5),
                 ),
                 Expanded(
-                    child: Container(
-                  width: 40,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          blurRadius: 8,
+                          spreadRadius: 2,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment
+                          .start, // جعل المحتوى يبدأ من اليسار
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.blue.shade100,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: const Icon(Icons.warning,
+                              color: Colors.blue, size: 20),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          "360",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        const Text(
+                          "Low stock",
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                )),
+                ),
                 const Padding(
                   padding: EdgeInsets.all(5),
                 ),
                 Expanded(
-                    child: Container(
-                  width: 40,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          blurRadius: 8,
+                          spreadRadius: 2,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment
+                          .start, // جعل المحتوى يبدأ من اليسار
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.blue.shade100,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: const Icon(Icons.shopping_basket,
+                              color: Colors.blue, size: 20),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          "360",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        const Text(
+                          "Total items",
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                )),
+                ),
               ],
             ),
+            const Padding(
+              padding: EdgeInsets.all(5),
+            ),
+            Row(children: [
+              const Expanded(
+                  child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 15), // مسافة من اليسار
+                  child: Text(
+                    " Recent Documents",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              )),
+              const Padding(
+                padding: EdgeInsets.all(5),
+              ),
+              Expanded(
+                child: Align(
+                  // محاذاة النص لليمين
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(right: 15), // مسافة من اليمين
+                    child: Text(
+                      " View All",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+            const Padding(
+              padding: EdgeInsets.all(10),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                blurRadius: 8,
+                                spreadRadius: 2,
+                                offset: const Offset(0, 4),
+                              ),
+                            ],
+                          ),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              // صورة المنتج مع معالجة الخطأ
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(12),
+                                child: Image.network(
+                                  "https://via.placeholder.com/60", // رابط صورة صالح
+                                  width: 60,
+                                  height: 60,
+                                  fit: BoxFit.cover,
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return const Icon(Icons.broken_image,
+                                        size: 60,
+                                        color:
+                                            Color.fromARGB(255, 243, 232, 231));
+                                  },
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+                              // النصوص
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      "Coca Cola",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Row(
+                                      children: [
+                                        // صورة الشخص مع معالجة الخطأ
+                                        CircleAvatar(
+                                          radius: 10,
+                                          backgroundImage: NetworkImage(
+                                              "https://via.placeholder.com/40"),
+                                          onBackgroundImageError: (_, __) =>
+                                              debugPrint(
+                                                  "Failed to load image"),
+                                          child: const Icon(Icons.person,
+                                              size: 12),
+                                        ),
+                                        const SizedBox(width: 6),
+                                        const Text(
+                                          "Silvia Thornton",
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 4),
+                                    const Text(
+                                      "8:30 AM",
+                                      style: TextStyle(
+                                          fontSize: 12, color: Colors.grey),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              // أيقونة النمو
+                              const Icon(Icons.trending_up,
+                                  color: Colors.green, size: 24),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(5),
+                      ),
+                    ],
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(5),
+                  ),
+                  Row(children: [
+                    Expanded(
+                        child: Container(
+                      width: 40,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    )),
+                    const Padding(
+                      padding: EdgeInsets.all(5),
+                    ),
+                  ]),
+                  const Padding(
+                    padding: EdgeInsets.all(5),
+                  ),
+                  Row(children: [
+                    Expanded(
+                        child: Container(
+                      width: 40,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    )),
+                    const Padding(
+                      padding: EdgeInsets.all(5),
+                    ),
+                  ]),
+                  const Padding(
+                    padding: EdgeInsets.all(5),
+                  ),
+                  Row(children: [
+                    Expanded(
+                        child: Container(
+                      width: 40,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    )),
+                    const Padding(
+                      padding: EdgeInsets.all(5),
+                    ),
+                  ]),
+                ],
+              ),
+            )
           ],
         ),
       ),
